ded33400bb7215277fe6125d07c5491f5d0b64ce
Miguel Franco <francamente@hotmail.es>
2023-12-03 02:24:07 +0100

    maquetacion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ProfesionalesComponent } from './pages/profesionales/profesionales.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: HomeComponent},
+  {path: "home", component: HomeComponent},
+  {path: "profesionales", component: ProfesionalesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
